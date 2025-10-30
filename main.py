82e2cf6f890386a3d7e38bf4854d1e9563292889
Walter Duarte <walterdu98@gmail.com>
2025-10-30 17:24:18 -0300

    Agregando main

---
+++ main.py
@@ -0,0 +1,5 @@
+# Archivo principal del grupo
+# Cada integrante deberá agregar su función en un archivo independiente
+from funciones import *
+if __name__ == "__main__":
+ print("Ejecución de pruebas de las funciones del grupo")
