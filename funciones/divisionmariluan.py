abf3abf1a164bdaa081ca365e8883d356ca71e02
leandro <leanmariluan@gmail.com>
2025-11-05 16:49:46 -0300

    trabajo grupal de MS2 mariluan

--- funciones/divisionmariluan.py
+++ funciones/divisionmariluan.py
@@ -1,5 +1,5 @@
 # funciones/divisionmariluan.py
-def divisionmariluan(a, b):
+def dividir(a, b):
  """Devuelve la división de dos números. Si b es 0, devuelve None."""
  if b == 0:
      return None
--- tests/test_divisionmariluan.py
+++ tests/test_divisionmariluan.py
@@ -1,12 +1,5 @@
-# tests/test_divisionmariluan.py
-from funciones.divisionmariluan import divisionmariluan
-
-def test_divisionmariluan():
- # división normal
- assert divisionmariluan(10, 2) == 5
-
- # division número impar
- assert divisionmariluan(5, 2) == 2.5
-
- # división por cero 
- assert divisionmariluan(5, 0) is None
+#tests/test_dividir.py
+from funciones.divisionmariluan import dividir
+def test_dividir():
+ assert dividir(10, 2) == 5
+ assert dividir(5, 0) is None
